9bf14275d642c50e27857ca105f947ac21b2570b
Phạm Thanh Dũng <143075225+NinjaThanh@users.noreply.github.com>
2026-06-30 09:19:20 +0700

    Create ticktickapp_FINAL1_HabitsPage.dart

---
+++ ticktickapp_FINAL1_HabitsPage.dart
@@ -0,0 +1,137 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+class HabitsPage extends StatelessWidget {
+  final User? user;
+  final List<bool> completed = const [
+    true,
+    true,
+    true,
+    true,
+    true,
+    false,
+    true,
+  ];
+
+  const HabitsPage({super.key, this.user});
+
+  @override
+  Widget build(BuildContext context) {
+    final days = ['M', 'T', 'W', 'T', 'F', 'S', 'S'];
+    return Scaffold(
+      body: Column(
+        children: [
+          Padding(padding: const EdgeInsets.only(top: 45.0)),
+          Padding(padding:EdgeInsets.all(16.0),
+            child: Container(
+              padding: EdgeInsets.all(20),
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(18),
+                border: Border.all(color: Colors.grey.shade300),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.grey.withOpacity(0.1),
+                    blurRadius: 10,
+                    offset: Offset(0,5 ),
+                  ),
+                ],
+              ),
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Expanded(
+                          child: Text("WEEKLY PROGRESS",
+                            style: TextStyle(
+                              fontWeight: FontWeight.w600,
+                              letterSpacing: 2,
+                              color: Colors.black54,
+                            ),
+                            overflow: TextOverflow.ellipsis,
+                          ),
+                      ),
+                      //
+                      const SizedBox(width: 8),
+                      Row(
+                        children: [
+                          Icon(
+                            Icons.local_fire_department,
+                            color: Colors.green,
+                          ),
+                          Text(
+                            "12 DAY STREAK",
+                            style: TextStyle(
+                              color: Colors.green,
+                              fontWeight: FontWeight.bold,
+                              letterSpacing: 2,
+                            ),
+                          )
+                        ],
+                      )
+                    ],
+                  ),
+                  //
+                  const SizedBox(height: 24),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: days
+                      .map((e) => Text(
+                      e,
+                      style: TextStyle(
+                        color: Colors.grey,
+                        fontWeight: FontWeight.w600,
+                      ),
+                    )
+                    ).toList(),
+                  ),
+                  const SizedBox(height: 16),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: List.generate(
+                      7,
+                        (index){
+                          if (index == 6){
+                          return Container(
+                            width: 20,
+                            height: 20,
+                            decoration: BoxDecoration(
+                              shape: BoxShape.circle,
+                              border: Border.all(
+                                color: Colors.blue.shade100,
+                                width: 5,
+                              ),
+                            ),
+                            child: Container(
+                              decoration: BoxDecoration(
+                                shape: BoxShape.circle,
+                                color: Colors.blue,
+                              ),
+                            ),
+                          );
+                        }return Container(
+                            width: 18,
+                            height: 18,
+                            decoration: BoxDecoration(
+                              shape: BoxShape.circle,
+                              color: completed[index] ? Colors.green : Colors.white,
+                              border: Border.all(
+                                color: completed[index]? Colors.green : Colors.grey.shade400,
+                              )
+                            ),
+                          );
+                        }
+                    ),
+                  )
+                ],
+              ),
+            ),
+          ),
+
+        ],
+      ),
+    );
+  }
+}
